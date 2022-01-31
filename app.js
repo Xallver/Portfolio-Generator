f56a3b1c64f8a06d1a7ce0c83c9cfe54e4eba110
Xallver <jamesstanley11@gmail.com>
2022-01-30 23:24:11 -0500

    captured user input

---
+++ app.js
@@ -0,0 +1,33 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+  // console.log(profileDataArgs);
+
+
+const printProfileData = profileDataArr => {
+  // this...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }  
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
+  
+ 
+
+// const message = 'Hello Node!';
+
+// if (true === true) {
+//   const message = 'Hello ES6!';
+//   let sum = 5;
+//   sum += 10;
+//   console.log(message);
+//   console.log(sum);
+// }
+
+// console.log(message);
+
+
