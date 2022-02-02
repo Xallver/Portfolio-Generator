20be900c04ffa2d4c917ea4911b042abbb409d7a
Xallver <jamesstanley11@gmail.com>
2022-02-01 23:08:04 -0500

    generated the HTML file

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-  // console.log(profileDataArgs);
+const fs = require('fs');
 
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-  // this...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }  
+const profileDataArgs = process.argv.slice(2);
 
-  console.log('================');
+const [userName, github] = profileDataArgs;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('index.html', generatePage(userName, github), err => {
+  if (err) throw err;
 
-printProfileData(profileDataArgs);
-  
- 
-
-// const message = 'Hello Node!';
-
-// if (true === true) {
-//   const message = 'Hello ES6!';
-//   let sum = 5;
-//   sum += 10;
-//   console.log(message);
-//   console.log(sum);
-// }
-
-// console.log(message);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 
