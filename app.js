8db2584b677f33f39ce5f98215e217f55fe94433
Xallver <jamesstanley11@gmail.com>
2022-02-04 19:51:50 -0500

    created generate projects function to take users input and create dynamic html code

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -25,7 +27,7 @@ const promptUser = () => {
         } else {
           console.log('Please enter your GitHub username!');
           return false;
-        }
+        } 
       }
     },
     {
@@ -136,18 +138,15 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+    
+    });
+    
   });
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
 
 
-// const pageHTML = generatePage(userName, github);
-
-// fs.writeFile('index.html', generatePage(userName, github), err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 
